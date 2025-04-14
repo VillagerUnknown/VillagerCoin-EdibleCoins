46f991759bcc71b87b36e9b182bcf7ef9cf688ff
villagerunknown <VillagerUnknown@proton.me>
2025-04-14 16:49:55 -0400

    Updates for release

--- src/main/java/me/villagerunknown/ediblecoins/EdiblecoinsConfigData.java
+++ src/main/java/me/villagerunknown/ediblecoins/EdiblecoinsConfigData.java
@@ -6,6 +6,11 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 @Config(name = "villagerunknown-ediblecoins")
 public class EdiblecoinsConfigData implements me.shedaniel.autoconfig.ConfigData {
 	
-
+	/**
+	 * General
+	 */
+	
+	@ConfigEntry.Category("General")
+	public boolean enableTheUnknown = false;
 	
 }
--- src/main/java/me/villagerunknown/ediblecoins/item/EdibleCoinItems.java
+++ src/main/java/me/villagerunknown/ediblecoins/item/EdibleCoinItems.java
@@ -4,6 +4,7 @@ import me.villagerunknown.villagercoin.feature.CoinFeature;
 import me.villagerunknown.villagercoin.feature.EdibleCoinFeature;
 import me.villagerunknown.villagercoin.feature.MobsDropCoinsFeature;
 import me.villagerunknown.villagercoin.feature.StructuresIncludeCoinsFeature;
+import net.minecraft.component.type.FoodComponents;
 import net.minecraft.item.Item;
 
 public class EdibleCoinItems {
@@ -14,6 +15,8 @@ public class EdibleCoinItems {
 	public static final Item EDIBLE_EMERALD_COIN;
 	public static final Item EDIBLE_NETHERITE_COIN;
 	
+	public static final Item POISONOUS_EDIBLE_COPPER_COIN;
+	
 	public EdibleCoinItems() {}
 	
 	static {
@@ -22,6 +25,8 @@ public class EdibleCoinItems {
 		EDIBLE_GOLD_COIN = EdibleCoinFeature.registerEdibleCoinItem( "edible_gold_" + CoinFeature.COIN_STRING, EdibleCoinFeature.GOLD_FOOD, CoinFeature.GOLD_RARITY, CoinFeature.GOLD_DROP_MINIMUM, EdibleCoinFeature.GOLD_DROP_MAXIMUM, EdibleCoinFeature.GOLD_DROP_CHANCE, StructuresIncludeCoinsFeature.UNCOMMON_LOOT_TABLES, MobsDropCoinsFeature.COMMON_MOB_DROPS );
 		EDIBLE_EMERALD_COIN = EdibleCoinFeature.registerEdibleCoinItem( "edible_emerald_" + CoinFeature.COIN_STRING, EdibleCoinFeature.EMERALD_FOOD, CoinFeature.EMERALD_RARITY, CoinFeature.EMERALD_DROP_MINIMUM, EdibleCoinFeature.EMERALD_DROP_MAXIMUM, EdibleCoinFeature.EMERALD_DROP_CHANCE, StructuresIncludeCoinsFeature.RARE_LOOT_TABLES, MobsDropCoinsFeature.RARE_MOB_DROPS );
 		EDIBLE_NETHERITE_COIN = EdibleCoinFeature.registerEdibleCoinItem( "edible_netherite_" + CoinFeature.COIN_STRING, EdibleCoinFeature.NETHERITE_FOOD, CoinFeature.NETHERITE_RARITY, CoinFeature.NETHERITE_DROP_MINIMUM, EdibleCoinFeature.NETHERITE_DROP_MAXIMUM, EdibleCoinFeature.NETHERITE_DROP_CHANCE, StructuresIncludeCoinsFeature.EPIC_LOOT_TABLES, MobsDropCoinsFeature.EPIC_MOB_DROPS );
+		
+		POISONOUS_EDIBLE_COPPER_COIN = EdibleCoinFeature.registerEdibleCoinItem( "poisonous_edible_copper_" + CoinFeature.COIN_STRING, FoodComponents.POISONOUS_POTATO, CoinFeature.COPPER_RARITY, CoinFeature.NETHERITE_DROP_MINIMUM, EdibleCoinFeature.NETHERITE_DROP_MAXIMUM, EdibleCoinFeature.GOLD_DROP_CHANCE, StructuresIncludeCoinsFeature.COMMON_LOOT_TABLES, MobsDropCoinsFeature.COMMON_MOB_DROPS );
 	}
 	
 }
