69aeaf49053bdb4025521a730e7d8c859b387ed4
villagerunknown <VillagerUnknown@proton.me>
2026-08-07 19:42:01 -0400

    Updating to Minecraft 26.1

--- src/main/java/me/villagerunknown/ediblecoins/item/EdibleCoinItems.java
+++ src/main/java/me/villagerunknown/ediblecoins/item/EdibleCoinItems.java
@@ -6,9 +6,8 @@ import me.villagerunknown.villagercoin.feature.CoinFeature;
 import me.villagerunknown.villagercoin.feature.EdibleCoinFeature;
 import me.villagerunknown.villagercoin.feature.MobsDropCoinsFeature;
 import me.villagerunknown.villagercoin.feature.StructuresIncludeCoinsFeature;
-import net.minecraft.component.type.FoodComponents;
-import net.minecraft.item.Item;
-
+import net.minecraft.world.food.Foods;
+import net.minecraft.world.item.Item;
 import java.util.List;
 
 public class EdibleCoinItems {
@@ -45,8 +44,8 @@ public class EdibleCoinItems {
 		EDIBLE_EMERALD_COIN = EdibleCoinFeature.registerEdibleCoinItem( Villagercoin.MOD_ID, "edible_emerald_" + CoinFeature.COIN_STRING, EdibleCoinFeature.EMERALD_FOOD, CoinFeature.EMERALD_RARITY, 0, EdibleCoinFeature.EMERALD_DROP_MAXIMUM, EdibleCoinFeature.NETHERITE_DROP_CHANCE, 1, 1, 1, StructuresIncludeCoinsFeature.EMERALD_LOOT_TABLES );
 		EDIBLE_NETHERITE_COIN = EdibleCoinFeature.registerEdibleCoinItem( Villagercoin.MOD_ID, "edible_netherite_" + CoinFeature.COIN_STRING, EdibleCoinFeature.NETHERITE_FOOD, CoinFeature.NETHERITE_RARITY, 0, EdibleCoinFeature.NETHERITE_DROP_MAXIMUM, EdibleCoinFeature.NETHERITE_DROP_CHANCE, 1, 1, 1, StructuresIncludeCoinsFeature.NETHERITE_LOOT_TABLES );
 		
-		HALF_EATEN_EDIBLE_COPPER_COIN = EdibleCoinFeature.registerEdibleCoinItem( Villagercoin.MOD_ID, "half_eaten_edible_copper_" + CoinFeature.COIN_STRING, FoodComponents.POTATO, CoinFeature.COPPER_RARITY, 0, 1, EdibleCoinFeature.IRON_DROP_CHANCE, 1, 1, 1, StructuresIncludeCoinsFeature.COPPER_LOOT_TABLES );
-		POISONOUS_EDIBLE_COPPER_COIN = EdibleCoinFeature.registerEdibleCoinItem( Villagercoin.MOD_ID, "poisonous_edible_copper_" + CoinFeature.COIN_STRING, FoodComponents.POISONOUS_POTATO, CoinFeature.COPPER_RARITY, 0, 1, EdibleCoinFeature.GOLD_DROP_CHANCE, 1, 1, 1, StructuresIncludeCoinsFeature.COPPER_LOOT_TABLES, List.of(StewEffects.POISON) );
+		HALF_EATEN_EDIBLE_COPPER_COIN = EdibleCoinFeature.registerEdibleCoinItem( Villagercoin.MOD_ID, "half_eaten_edible_copper_" + CoinFeature.COIN_STRING, Foods.POTATO, CoinFeature.COPPER_RARITY, 0, 1, EdibleCoinFeature.IRON_DROP_CHANCE, 1, 1, 1, StructuresIncludeCoinsFeature.COPPER_LOOT_TABLES );
+		POISONOUS_EDIBLE_COPPER_COIN = EdibleCoinFeature.registerEdibleCoinItem( Villagercoin.MOD_ID, "poisonous_edible_copper_" + CoinFeature.COIN_STRING, Foods.POISONOUS_POTATO, CoinFeature.COPPER_RARITY, 0, 1, EdibleCoinFeature.GOLD_DROP_CHANCE, 1, 1, 1, StructuresIncludeCoinsFeature.COPPER_LOOT_TABLES, List.of(StewEffects.POISON) );
 		MOLDY_EDIBLE_GOLD_COIN = EdibleCoinFeature.registerEdibleCoinItem( Villagercoin.MOD_ID, "moldy_edible_gold_" + CoinFeature.COIN_STRING, EdibleCoinFeature.GOLD_FOOD, CoinFeature.GOLD_RARITY, 0, 1, EdibleCoinFeature.GOLD_DROP_CHANCE, 1, 1, 1, StructuresIncludeCoinsFeature.GOLD_LOOT_TABLES, List.of(StewEffects.FIRE_RESISTANCE,StewEffects.BLINDNESS,StewEffects.SATURATION,StewEffects.NAUSEA,StewEffects.JUMP_BOOST,StewEffects.POISON,StewEffects.REGENERATION,StewEffects.NIGHT_VISION,StewEffects.WEAKNESS,StewEffects.WITHER) );
 	}
 	
